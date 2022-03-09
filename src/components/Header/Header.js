ec9439b3322fed30c14a43a3cc1379a328a28d5a
David Vergaray <david.vergaray.a@gmail.com>
2022-03-09 17:48:09 -0300

    Finish style header, add fonts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import backArrow from '../../assets/icons/back-arrow.png';
 import mic from '../../assets/icons/microphone.png';
 import settings from '../../assets/icons/settings.png';
+import '../../assets/Fonts.css';
 
 const Container = styled.header`
   padding: 0.5rem;
-  background-color: red;
+  background-color: #4369b2;
 `;
 
 const Nav = styled.nav`
@@ -19,37 +21,50 @@ const Back = styled.div`
   align-items: center;
 `;
 
-const Year = styled.p`
-  margin: 0;
-`;
-
-const Category = styled.div`
-
-`;
+const TitleStyle = {
+  color: 'white',
+  fontSize: '0.8rem',
+  fontFamily: 'Lato',
+  textDecoration: 'none',
+};
 
 const Options = styled.div`
   display: flex;
   align-items: center;
 `;
 
+const arrowStyle = {
+  height: '1.4rem',
+  width: '1.4rem',
+  filter: 'invert(100)',
+};
+
+const micStyle = {
+  height: '1.1rem',
+  width: '1.1rem',
+  paddingRight: '1rem',
+  filter: 'invert(100)',
+};
+
 const iconStyle = {
-  height: '2rem',
-  width: '2rem',
+  height: '1.1rem',
+  width: '1.1rem',
+  filter: 'invert(100)',
 };
 
 const Header = () => {
-  const BackIcon = () => <img src={backArrow} style={iconStyle} alt="back-icon" />;
-  const MicIcon = () => <img src={mic} style={iconStyle} alt="microphone-icon" />;
+  const MicIcon = () => <img src={mic} style={micStyle} alt="microphone-icon" />;
+  const BackIcon = () => <img src={backArrow} style={arrowStyle} alt="back-icon" />;
   const SettingsIcon = () => <img src={settings} style={iconStyle} alt="settings-icon" />;
 
   return (
     <Container>
       <Nav>
         <Back>
-          <BackIcon />
-          <Year>2022</Year>
+          <Link to="/"><BackIcon /></Link>
+          {/* <Year>2022</Year> */}
         </Back>
-        <Category>Daily Covid-19</Category>
+        <Link to="/" style={TitleStyle}>Daily Covid-19</Link>
         <Options>
           <MicIcon />
           <SettingsIcon />
